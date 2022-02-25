8d25397053088d712ef95ef92cb22caf152f1036
Guilherme Yuji Koyama Soken <guilhermesoken2001@gmail.com>
2022-02-25 12:20:41 -0300

    Interfaces DepartmentDao e SellerDao

--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -1,7 +1,15 @@
 package model.dao;
 
+import java.util.List;
+
+import model.entites.Department;
+
 public interface DepartmentDao {
 
 	
-	
+	void insert(Department obj);
+	void update(Department obj);
+	void deleteById(Integer id);
+	Department findyById(Integer id);
+	List<Department> findAll();
 }

a757907295735adf24c2ba6f9f122137e0a7c46a
Guilherme Yuji Koyama Soken <guilhermesoken2001@gmail.com>
2022-03-02 13:02:50 -0300

    Delete

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,6 +12,8 @@ import model.entites.Seller;
 public class Program {
 
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
@@ -42,6 +45,14 @@ public class Program {
 		seller.setName("Bruce Wayne");
 		sellerDao.update(seller);
 		System.out.println("Atualizado!");
+		
+		System.out.println("\n--- Test 6: Seller delete ---");
+		System.out.println("Coloque o id do vendedor que quer excluir:");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Deletado!");
+		
+		sc.close();
 	}
 	
 
